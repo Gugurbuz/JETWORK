a5ce8ff9b8f3f3e5ae75de876347489aafbdcb2e
Gugurbuz <gurkangurbuz@hotmail.com.tr>
2026-08-23 15:45:09 +0300

    feat: expose evidence-aware knowledge tools to auto core

---
+++ supabase/functions/_shared/assistantToolsEvidenceAware.ts
@@ -0,0 +1,6 @@
+import * as technical from './assistantToolsTechnicalReferenceQuality.ts'
+
+export * from './assistantToolsTechnicalReferenceQuality.ts'
+
+export const ASSISTANT_KNOWLEDGE_TOOLS = technical.ASSISTANT_KNOWLEDGE_TOOLS
+export const executeAssistantTool = technical.executeAssistantTool
